e9b7748a9279085f193da65b76dc5f811f28b840
kyle-mcknight <kyle.mcknight@accesso.com>
2020-08-13 12:01:11 -0400

    Added ability to have multiple controlled icons and to choose one that gets the loot

---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+// get the distance to the token and if it's too far then can't pick it up
+export const dist = (p1: PlaceableObject, p2: PlaceableObject): number => {
+  return Math.hypot(p1.x - p2.x, p1.y - p2.y);
+};
